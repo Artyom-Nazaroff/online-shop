9bd4dfb67438c92a9874e9cf46bb64fa183b255c
Artyom Nazaroff <artyom.nazaroff@yandex.ru>
2026-08-01 18:39:47 +0800

    feat: initialize Prisma service, configure CORS, and enable cookie parsing in the backend application

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  app.use(cookieParser())
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    exposedHeaders: 'Set-Cookie'
+  })
+  await app.listen(process.env.SERVER_URL ?? 5000)
+  console.log(`Server is running: ${process.env.SERVER_URL}`)
 }
-bootstrap();
+bootstrap()
